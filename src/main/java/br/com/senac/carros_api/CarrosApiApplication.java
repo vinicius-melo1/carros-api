8941928622b13d2b7d523ace1ad2d1214277eb0e
VINICIUS NASCIMENTO DE MELO <vinicius.melo2@alunos.sc.senac.br>
2026-04-22 20:07:56 -0300

    segundo commit

---
+++ src/main/java/br/com/senac/carros_api/CarrosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.carros_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senac/carros_api/CarrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.carros_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
